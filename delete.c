e46eebf4b5945729fa6304ec7130baed87a9f9c6
jdbenavente <daniel.benavent3@gmail.com>
2025-02-18 22:51:28 -0700

    changed the c file so that I can show that process do continue running after &

--- delete.c
+++ delete.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <unistd.h> // Include for sleep()
+
 int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+    sleep(5); // Sleep for 5 seconds
+    printf("Hello, World!\n"); // Added a newline character for cleaner output
+    return 0;
 }
